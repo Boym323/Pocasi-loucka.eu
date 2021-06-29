b2961d71f0f2770e04d897c644ce5ab55af5f120
Boym323 <martin@pomykal.cz>
2021-06-29 16:47:13 +0200

    Doplněny měření ze střechy

--- Senzor strecha/src/main.cpp
+++ Senzor strecha/src/main.cpp
@@ -1,10 +1,43 @@
 #include "painlessMesh.h"
 #include "credentials.h"
 
+#include <ADS1115_WE.h>
+#include <Wire.h>
+
+ADS1115_WE adc = ADS1115_WE(0x48);
+
 Scheduler userScheduler; // to control your personal task
 painlessMesh mesh;
-int casOdeslani = 20; //sekund
+int casOdeslani = 30; //sekund
+//weather sensors
+const byte windSpeedPin = 12;
+const byte rainPin = 14;
+volatile unsigned int windcnt = 0;
+volatile unsigned int raincnt = 0;
+volatile unsigned long lastRainClick;
+volatile unsigned long lastWindClick;
+float windspeed;
+float r;
+float napetiWindDir;
+String windDir;
 
+IRAM_ATTR void WindSpeed()
+{
+  if ((millis() - lastWindClick > 16))
+  { // debounce of sensor signal
+    windcnt++;
+    lastWindClick = millis();
+  }
+}
+
+IRAM_ATTR void Rain()
+{
+  if ((millis() - lastRainClick > 500))
+  { // debounce of sensor signal
+    raincnt++;
+    lastRainClick = millis();
+  }
+}
 // Prototype
 void receivedCallback(uint32_t from, String &msg);
 
@@ -12,16 +45,60 @@ size_t logServerId = 0;
 
 // Send message to the logServer every 10 seconds
 
+float readChannel(ADS1115_MUX channel)
+{
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  voltage = adc.getResult_V(); // alternative: getResult_mV for Millivolt
+  return voltage;
+}
+
 Task nacteniDatCidla(casOdeslani * 1000, TASK_FOREVER, []()
                      {
-                     /*  humSi7021 = Si7021.readHumidity();
-                       tempSi7021 = Si7021.readTemperature();
-                       DeviceAddress Teplota2m = {0x28, 0xFF, 0x64, 0x1D, 0xF9, 0x86, 0x3C, 0x78};
-                       sensors.setResolution(Teplota2m, 11);
-                       //Serial.print(sensors.getResolution(Teplota2m), DEC);
+                       //Calculate Wind Speed (klicks/interval * 2,4 kmh)
+                       windspeed = ((windcnt / casOdeslani) * 2.4) * (1 / 3.6);
+                       windcnt = 0;
+                       //Calculate Rain
+                       r = raincnt * 0.2794;
+                       raincnt = 0;
+                       //Calculate win direction
+                       napetiWindDir = readChannel(ADS1115_COMP_0_GND);
+                       if (napetiWindDir < 0.3216)
+                         windDir = "112.5";
+                       else if (napetiWindDir < 0.4092)
+                         windDir = "67.5";
+                       else if (napetiWindDir < 0.4545)
+                         windDir = "90";
+                       else if (napetiWindDir < 0.6166)
+                         windDir = "157.5";
+                       else if (napetiWindDir < 0.9016)
+                         windDir = "135";
+                       else if (napetiWindDir < 1.1936)
+                         windDir = "202.5";
+                       else if (napetiWindDir < 1.4029)
+                         windDir = "180";
+                       else if (napetiWindDir < 1.9821)
+                         windDir = "22.5";
+                       else if (napetiWindDir < 1.2527)
+                         windDir = "45";
+                       else if (napetiWindDir < 2.9268)
+                         windDir = "247.5";
+                       else if (napetiWindDir < 3.0769)
+                         windDir = "225";
+                       else if (napetiWindDir < 3.4314)
+                         windDir = "337.5";
+                       else if (napetiWindDir < 3.8372)
+                         windDir = "0";
+                       else if (napetiWindDir < 4.0407)
+                         windDir = "292.5";
+                       else if (napetiWindDir < 4.3324)
+                         windDir = "315";
+                       else if (napetiWindDir < 4.6154)
+                         windDir = "270";
+                       else
+                         windDir = "0";
+                     });
 
-                       sensors.requestTemperatures();
-                       tempDS18B20 = sensors.getTempC(Teplota2m);*/ });
 Task myLoggingTask(casOdeslani * 1000, TASK_FOREVER, []()
                    {
                      DynamicJsonDocument jsonBuffer(1024);
@@ -49,7 +126,14 @@ void setup()
 {
   Serial.begin(115200);
 
-  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
+  Wire.begin();
+  adc.setVoltageRange_mV(ADS1115_RANGE_6144); //comment line/change parameter to change range
+  adc.setCompareChannels(ADS1115_COMP_0_GND);
+  adc.setMeasureMode(ADS1115_SINGLE);
+  pinMode(windSpeedPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(windSpeedPin), WindSpeed, FALLING);
+  pinMode(rainPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(rainPin), Rain, RISING);
 
   mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_AP_STA, 6);
   mesh.onReceive(&receivedCallback);
